1906d9a086f06831fa1bb96fd5fdd6561da465b9
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-01-23 01:47:14 +0900

    add phar etc.

--- scripts/change-php-version-example.sh
+++ scripts/change-php-version-example.sh
@@ -14,12 +14,24 @@ showMessage() {
 
 # unlink
 showMessage 'Unlink Current Symbolic Link'
+unlink /opt/homebrew/bin/phar
+unlink /opt/homebrew/bin/phar.phar
 unlink /opt/homebrew/bin/php
+unlink /opt/homebrew/bin/php-cgi
+unlink /opt/homebrew/bin/php-config
+unlink /opt/homebrew/bin/phpdbg
+unlink /opt/homebrew/bin/phpize
 unlink /opt/homebrew/sbin/php-fpm
 
 # make symbolic link
 showMessage `make Symbolic Link of PHP ${PHP_FULL_VERSION}`
+ln -svf /opt/homebrew/Cellar/${PHP_MAJOR_VERSION}/${PHP_FULL_VERSION}/bin/phar /opt/homebrew/bin/phar
+ln -svf /opt/homebrew/Cellar/${PHP_MAJOR_VERSION}/${PHP_FULL_VERSION}/bin/phar.phar /opt/homebrew/bin/phar.phar
 ln -svf /opt/homebrew/Cellar/${PHP_MAJOR_VERSION}/${PHP_FULL_VERSION}/bin/php /opt/homebrew/bin/php
+ln -svf /opt/homebrew/Cellar/${PHP_MAJOR_VERSION}/${PHP_FULL_VERSION}/bin/php-cgi /opt/homebrew/bin/php-cgi
+ln -svf /opt/homebrew/Cellar/${PHP_MAJOR_VERSION}/${PHP_FULL_VERSION}/bin/php-config /opt/homebrew/bin/php-config
+ln -svf /opt/homebrew/Cellar/${PHP_MAJOR_VERSION}/${PHP_FULL_VERSION}/bin/phpdbg /opt/homebrew/bin/phpdbg
+ln -svf /opt/homebrew/Cellar/${PHP_MAJOR_VERSION}/${PHP_FULL_VERSION}/bin/phpize /opt/homebrew/bin/phpize
 ln -svf /opt/homebrew/Cellar/${PHP_MAJOR_VERSION}/${PHP_FULL_VERSION}/sbin/php-fpm /opt/homebrew/sbin/php-fpm
 
 # restart
