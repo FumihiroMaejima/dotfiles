971ee99704a5704685bd17e7634e261832d50ee7
Fumihiro_Maejima <testerwatson1101@gmail.com>
2023-09-18 18:27:38 +0900

    fix value name.

--- scripts/dev/update-composer-example.sh
+++ scripts/dev/update-composer-example.sh
@@ -2,12 +2,12 @@
 
 # CURRENT_DIR=$(cd $(dirname $0); pwd)
 
-SUDO_PASSWORD=test_password
+PASSWORD=test_password
 
 # openssl がインストールされていない場合
 # brew install openssl
 
 curl -sS https://getcomposer.org/installer | php
 # sudoで実行する必要がある
-echo ${SUDO_PASSWORD} | sudo -S mv composer.phar /usr/local/bin/composer
+echo ${PASSWORD} | sudo -S mv composer.phar /usr/local/bin/composer
 composer --version
