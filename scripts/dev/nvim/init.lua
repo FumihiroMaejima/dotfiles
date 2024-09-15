a700e3dc9aaac5b11e603459e7bdcccd7ce13541
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-09-16 01:42:27 +0900

    create lua dir.

--- scripts/dev/nvim/init.lua
+++ scripts/dev/nvim/init.lua
@@ -1,10 +1,3 @@
--- 文字コードをUTF-8
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
+print('init.lua')
 
--- 編集中のファイルに行番号を表示する
-vim.wo.number = true
-
--- 不必要なswapファイルを作成させない
-vim.opt.swapfile = false
+require("base")
