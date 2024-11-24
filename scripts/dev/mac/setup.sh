555352c09dad6a4dddcc1a4f205e58cb7a564b26
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-11-25 00:19:09 +0900

    add setup brew package.

--- scripts/dev/mac/setup.sh
+++ scripts/dev/mac/setup.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-function settingProfile() {
-  ### .zprofileに設定を反映(設定済みなら不要)
-  # echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
-  source $HOME/.zprofile
-}
-
+# homebrew
 if [[ "$(brew --version 2>/dev/null)" != "" ]]; then
   # インストール済みの場合
   echo "Alerady Installed."
   exit
+else
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  source $HOME/.zprofile
 fi
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# 利用するアプリケーション
+brew install --cask google-chrome
+brew install --cask slack
+# パッケージ
+brew install openssl
+brew install git-flow
+brew install jq
 
-settingProfile
