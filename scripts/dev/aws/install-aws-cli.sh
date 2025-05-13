1b029b842ca3619d2621dcf7b479898d95968cfd
Fumihiro_Maejima <testerwatson1101@gmail.com>
2025-05-14 01:00:59 +0900

    modify for mac OS.

--- scripts/dev/aws/install-aws-cli.sh
+++ scripts/dev/aws/install-aws-cli.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-if [[ "$(aws --version 2>/dev/null)" != "" ]]; then
-  # インストール済みの場合
-  echo "Alerady Installed."
-  exit
+PROFILE_FILE_NAME='.zprofile'
+
+IS_REPRACE=$1
+if [ "$IS_REPRACE" != "true" ]; then
+  if [[ "$(aws --version 2>/dev/null)" != "" ]]; then
+    # インストール済みの場合
+    echo "Alerady Installed."
+    exit
+  fi
 fi
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliV2.zip"
-unzip awscliV2.zip
-sudo ./aws/install
+### 公式のMac用のインストーラーをダウンロード
+curl "https://awscli.amazonaws.com/AWSCLIV2-2.0.30.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg ./AWSCLIV2.pkg -target /
+
+aws --verion
+
+### 不要かつそれなりに容量があるのでインストーラーを削除
+rm ./AWSCLIV2.pkg
